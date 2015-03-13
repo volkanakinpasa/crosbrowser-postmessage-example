8475b810438d3d2050a29441c0eb413c0948bb1b
volkanakinpasa <volkanakintr@gmail.com>
2015-03-13 23:40:54 +0200

    mini example about posting message between page and iframe

---
+++ domainiframe/eventer.js
@@ -0,0 +1,9 @@
+var listen = function(callback){
+    var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+    var eventer = window[eventMethod];
+    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+    eventer(messageEvent, function (e) {
+        callback(e);
+    }, false);
+}
